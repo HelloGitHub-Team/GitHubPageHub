7d8107af386c03d73df81d35356bf6d2f548847e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-30 15:23:09 +0000

    Deploy HelloGitHub-Team/GitHubPageHub to github.com/HelloGitHub-Team/GitHubPageHub.git:gh-pages

---
+++ hg-app/build/precache-manifest.c66c3a8e99731d1b5f56171e4d7479b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "369912974542f08370586a81dfd4a392",
+    "url": "/GitHubPageHub/build/index.html"
+  },
+  {
+    "revision": "9759438930c61b536b1a",
+    "url": "/GitHubPageHub/build/static/css/2.46439c26.chunk.css"
+  },
+  {
+    "revision": "c2d388ccc8a3f9b47454",
+    "url": "/GitHubPageHub/build/static/css/main.8a96d73e.chunk.css"
+  },
+  {
+    "revision": "9759438930c61b536b1a",
+    "url": "/GitHubPageHub/build/static/js/2.7bf936f0.chunk.js"
+  },
+  {
+    "revision": "c2d388ccc8a3f9b47454",
+    "url": "/GitHubPageHub/build/static/js/main.fed42bee.chunk.js"
+  },
+  {
+    "revision": "6b213861e60d1a235b6e",
+    "url": "/GitHubPageHub/build/static/js/runtime~main.822a7aa5.js"
+  }
+]);
