422704b8a8670e18fcd7435ad93e7112d5cd1fc2
ming995 <hymghost@gmail.com>
2019-07-30 17:35:27 +0800

    fix: fix bug

--- build/precache-manifest.d053685d068f6ad9f3cedf71448020e8.js
+++ build/precache-manifest.d053685d068f6ad9f3cedf71448020e8.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7ad7f8934f6885a0751c466b4e111433",
+    "revision": "62d7c3bffecd2ed191c598f3f26baac6",
     "url": "/node-wiki/build/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/node-wiki/build/static/css/2.46439c26.chunk.css"
   },
   {
-    "revision": "828691e9967cb3a0d578",
+    "revision": "302313747a5ba01f5de5",
     "url": "/node-wiki/build/static/css/main.8a96d73e.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/node-wiki/build/static/js/2.8cc6f53a.chunk.js"
   },
   {
-    "revision": "828691e9967cb3a0d578",
-    "url": "/node-wiki/build/static/js/main.19873c92.chunk.js"
+    "revision": "302313747a5ba01f5de5",
+    "url": "/node-wiki/build/static/js/main.130be4c1.chunk.js"
   },
   {
     "revision": "fcca0fb592538e091db9",
